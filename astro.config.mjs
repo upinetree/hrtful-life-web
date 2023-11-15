be0d83118c1f96ecbb1327a29224921623edf7e3
upinetree <upinetree@gmail.com>
2023-11-15 13:32:37 +0000

    enable prefetch for all links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
@@ -8,5 +8,6 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  prefetch: { prefetchAll: true },
 });
