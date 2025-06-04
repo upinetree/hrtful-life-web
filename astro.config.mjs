5a3b1206911a902aa0416d5106e691bbcd2c4210
upinetree <upinetree@gmail.com>
2025-06-04 14:23:49 +0000

    have been merged into the default static option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   adapter: cloudflare(),
   integrations: [tailwind(), mdx()],
 });
